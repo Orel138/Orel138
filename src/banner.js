64de014f92de6174865dd7430a211657fe5505cc
Orel138 <orel.reynaud@gmail.com>
2024-07-21 14:41:52 +0200

    🍱 (assets): add new light theme banner image (light-banner-2.png) ✨ (banner.js): update bannersLight array to include new light theme banner image for random selection

--- src/banner.js
+++ src/banner.js
@@ -1,5 +1,5 @@
 function getRandomBanner(isDarkTheme) {
-  const bannersLight = ['light-banner-1.png'];
+  const bannersLight = ['light-banner-1.png', 'light-banner-2.png'];
   const bannersDark = ['dark-banner-1.png'];
 
   // Choose the appropriate listing based on the theme
